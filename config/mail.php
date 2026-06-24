0a3957a312cea6077f000ece4fc91e6ffca4314f
vedantkhandale <vedantkhandale8@gmail.com>
2026-06-24 12:08:27 +0530

    updating email change mail

--- config/mail.php
+++ config/mail.php
@@ -2,8 +2,8 @@
 
 return [
     "host" => "smtp.gmail.com",
-    "username" => "vedantkhandale8@gmail.com",
-    "password" => "nfhy didc bgcz eoxk",
+    "username" => "AluminiX5@gmail.com",
+    "password" => "nfhy didc bgcz eoxk", // <-- Is purane password ko hatana hai
     "port" => 587,
     "encryption" => "tls",
     "from_name" => "AlumniX Portal",
